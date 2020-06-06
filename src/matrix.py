c4279f38513a08844d31951b5e75b1ca2e918adf
Aman Sharma <lunchspider03@gmail.com>
2020-06-06 23:54:07 +0530

    added some important methods

--- src/matrix.py
+++ src/matrix.py
@@ -12,22 +12,19 @@ class matrix() :
 	AND IS MATRIX NULL OR NOT IF IT IS NOT NULL SO YOU HAVE TO PROVIDE 
 	AN ITERABLE(MOST PROBABLY A LIST OR TUPLE) WHICH CAN BE USED TO 
 	ENTER ELEMENTS IN THE MATRIX
+	IF ONLY A ROW LIST OR A TUPLE IS GIVEN THE CONSTRUCTOR CREATE A COLUMN MATRIX OF THE GIVEN LENGHT
 	"""
-	def __init__(self , name , order = "1x1" , null = True , iterable =()) :
-		
-		self.name = name
-		self.order = order
-		self.rows = int(order.partition("x")[0])
-		self.columns = int(order.partition("x")[2])
-		
-		if (null == True) :
-			
+	def __init__(self , order = "" , null = False , iterable =()) :
+		if(null==False):
+			self.__row=0
+			self.__col=0
+			self.__order=0
 			self.__matrix = self.__null_matrix()
-			
-		else :
-			
+		else:
+			self.__row = len(iterable)
+			self.__col = len(iterable[0])
+			self.__order = __row * __col
 			self.__matrix = self.__put_into_matrix(iterable)
-		
 	"""
 	IF ON THE WAY AT ANY INSTANT IF YOU WANTS TO CHANGE THE WHOLE 
 	MATRIX OR WANTS TO CHANGE THE INTIALY GENERATED MATRIX YOU CAN 
@@ -39,14 +36,14 @@ class matrix() :
 		
 		print("USE SPACES TO SEPRATE DIFFERENT ELEMENTS")
 		
-		for row in range(1 , self.rows + 1) :
+		for row in range(1 , self.__row + 1) :
 				
 				self.__x = input(f"ENTER THE ELEMENTS OF ROW {row} :")
-				self.__x = self.__x.split(" " , self.columns)
+				self.__x = self.__x.split(" " , self.__col)
 				
-				if (len(self.__x) != self.columns) :
+				if (len(self.__x) != self.__col) :
 					
-					 print(f"NUMBER OF ELEMENTS IN ONE ROW CAN ONLY BE  {self.columns} BUT YOU GAVE {len(self.__x)}")
+					 print(f"NUMBER OF ELEMENTS IN ONE ROW CAN ONLY BE  {self.__col} BUT YOU GAVE {len(self.__x)}")
 					 self.__matrix = []
 					 break
 					 
@@ -69,9 +66,9 @@ class matrix() :
 		
 		for __x in iterable :
 			
-			if (len(__x) != self.columns) :
+			if (len(__x) != self.__col) :
 					
-					 print(f"NUMBER OF ELEMENTS IN ONE ROW CAN ONLY BE  {self.columns} BUT YOU GAVE {len(__x)}")
+					 print(f"NUMBER OF ELEMENTS IN ONE ROW CAN ONLY BE  {self.__col} BUT YOU GAVE {len(__x)}")
 					 __matrix = []
 					 break
 					 
@@ -86,26 +83,6 @@ class matrix() :
 		return __matrix
 				
 			
-	"""
-	THIS FUNCTION display() DISPLAYS THE MATRIX ON THE SCREEN IN A SPACIFIC 
-	MANNER WITH ITS NAME
-	"""	
-	def display(self) :
-		
-		if (len(self.__matrix) > 0) :
-				
-			print(f"MATRIX {self.name} IS :")
-			self.__str = ""
-				
-			for i in self.__matrix :
-					
-				self.__str += f"{i}\n"
-			
-			return self.__str
-			
-		else :
-			
-			print(f"NO CONTENT EXIST IN MARIX {self.name}")
 	
 	"""
 	THIS FUNCTION __null_matrix() WILL MAKE THE MATRIX AS NULL MATRIX 
@@ -116,16 +93,37 @@ class matrix() :
 		
 		self.__matrix = []
 		
-		for i in range(self.rows) :
+		for i in range(self.__row) :
 			
 			self.__list = []
 			
-			for j in range(self.columns) :
+			for j in range(self.__col) :
 				
 				self.__list += [0]
 				
 			self.__matrix += [self.__list]
-			
 		return self.__matrix
-			
 
+	def pullMatrix(self):
+		return self.__matrix
+
+	def pullRow(self,RowNumber):
+		return self.__matrix[RowNumber]
+
+	def pullCol(self,ColNumber):
+		return [[x[0] for x in self.__matrix], [x[1] for x in self.__matrix]]
+
+	def getRow(self):
+		return self.__rows
+	
+	def getCol(self):
+		return self.__col
+	
+	def getOrder(self):
+		return self.__order
+	
+	"""
+	This method converts the matrix object into string.
+	"""
+	def __str__(self):
+		return str(self.__matrix)
--- src/test_file.py
+++ src/test_file.py
@@ -1,7 +1,4 @@
 from matrix import matrix
-
-a = matrix("A" , "2x3" , null = False , iterable = ((1,2,3) , (4,5,6)))
-
-#a.matrix_input()
-
-print(a.display())
+a = [[1,2,3],[1,2,3]]
+a = matrix(a)
+print(a)
