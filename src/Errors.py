cd4a11df04dd464f44ab31c34b64d62fa400483d
Aman Sharma <lunchspider03@gmail.com>
2020-06-08 23:26:04 +0530

    commit

--- src/Errors.py
+++ src/Errors.py
@@ -1,2 +1,5 @@
-class IntFloatError():
-    pass
+class IntFloatError(Exception):
+    def __init__(self,value):
+        self.value = value
+    def __str__(self):
+        return (self.value + " is not valid.\n Only integers and floats are allowed.")
