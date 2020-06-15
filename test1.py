0b7aac7c5574af86a78d29de9c2aa9438c26c37f
Aman Sharma <lunchspider03@gmail.com>
2020-06-15 23:32:40 +0530

    commit

--- test1.py
+++ test1.py
@@ -1,8 +1,4 @@
-from proton.matrices.decompose import *
-l = matrix([[1,3,5],[2,4,7],[1,1,0]])
-k = decompose.LU(l)
+from proton.matrices import matrix,decompose
+l =matrix.matrix([[1,2,3],[4,5,6],[7,8,9]])
+k = decompose.decompose.LU(l)
 print(k)
-lo = k[0]
-up = k[1]
-print(lo * up)
-print(k[2] * l)
