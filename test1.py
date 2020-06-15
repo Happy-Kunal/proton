7c6412ab4f0094d05df5d23f71a544bc8864b137
Kunal Sharma <kunalsharmakumar23@gmail.com>
2020-06-15 23:20:03 +0530

    added det.py to find det(matrix)

--- test1.py
+++ test1.py
@@ -1,8 +1,13 @@
 from proton.matrices.decompose import *
+from proton.linalge.det import det
+
 l = matrix([[1,3,5],[2,4,7],[1,1,0]])
-k = decompose.LU(l)
-print(k)
-lo = k[0]
-up = k[1]
-print(lo * up)
-print(k[2] * l)
+m = matrix([[1,2,3],[4,5,6],[7,8,9]])
+
+
+
+print(f"l = {l}")
+print(f"det(l) = {det(l)}")
+
+print(f"m = {m}")
+print(f"det(m) = {det(m)}")
